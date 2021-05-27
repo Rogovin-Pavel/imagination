4c4f17bff92e5bb59d76dc58dffeceff76032dce
p.rogovin <p.rogovin@devinotele.com>
2021-05-27 20:32:23 +0300

    production build

---
+++ docs/precache-manifest.cc5979f3544d67cee5328a84147871b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc9d406b2a97ab432ca",
+    "url": "/css/app.5d1ee258.css"
+  },
+  {
+    "revision": "1fc19942c08bd3a0fc564852e03621c4",
+    "url": "/img/me.1fc19942.jpg"
+  },
+  {
+    "revision": "af99026caded2feba4c641cb7ae896e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be404d849d30ac803585",
+    "url": "/js/about.5748e094.js"
+  },
+  {
+    "revision": "7fc9d406b2a97ab432ca",
+    "url": "/js/app.d3e2f888.js"
+  },
+  {
+    "revision": "22507b514c477bb8ca20",
+    "url": "/js/chunk-vendors.00eae411.js"
+  },
+  {
+    "revision": "4014f86645faf0d61e617ab89a328661",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
