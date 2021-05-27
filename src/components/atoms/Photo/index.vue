46f706f665eacbaf540fb062692b13c31575e931
p.rogovin <p.rogovin@devinotele.com>
2021-05-27 20:27:46 +0300

    cleaned up the initial state of the application

---
+++ src/components/atoms/Photo/index.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'Photo',
+  props: {
+    src: String,
+    alt: {
+      type: String,
+      default: 'photo',
+    },
+  },
+};
+</script>
+
+<template>
+  <img
+    :src="src"
+    :alt="alt"
+    :class="$style.root"
+  />
+</template>
+
+
+<style module>
+.root {
+  width: 400px;
+  height: 400px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
